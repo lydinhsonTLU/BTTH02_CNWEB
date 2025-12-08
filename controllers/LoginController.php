892dc8fcc869a4a72e5da37b990eca7d22f36bb9
Lý Đình Sơn <2351170615@e.tlu.edu.vn>
2025-12-08 23:36:36 +0700

    cập nhật đường dẫn

---
+++ controllers/LoginController.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ . '/../config/configRoute.php';
+
+include __DIR__ . '/../views/auth/login.php';
---
+++ controllers/RegisterController.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../config/configRoute.php';
+
+define("APP_ROOT", dirname(__DIR__));
+
+include APP_ROOT . '/views/auth/register.php';
